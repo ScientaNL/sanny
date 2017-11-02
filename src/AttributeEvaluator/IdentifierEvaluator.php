f7291f5134ac9ed492d84d415ef0d676a28e94dd
Harmen Mojet <h.mojet@syslogic.nl>
2017-11-02 11:23:34 +0100

    Fixing second pass whitespace artifacts. Now Sanitization of sanitized HTML results in exactly the same output

--- src/AttributeEvaluator/IdentifierEvaluator.php
+++ src/AttributeEvaluator/IdentifierEvaluator.php
@@ -6,8 +6,11 @@ class IdentifierEvaluator implements AttributeEvaluatorInterface
 {
 	public function __invoke(string $value)
 	{
-		//Allow space because people may use it in anchor ids...
-		return (preg_match('/^[_a-zA-Z0-9,][\\w:.\-, ()]*$/m', $value))
+		/**
+		 * Allow space because people may use it in anchor ids...
+		 * Allow percent because people may use spaces in name resulting in a %20
+		 */
+		return (preg_match('/^[_a-zA-Z0-9,][\\w:.\-, ()%]*$/m', $value))
 			? $value : false;
 	}
 }
